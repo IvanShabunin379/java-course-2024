db6a62ac7df2fd41a477d080741b88b7e78e3d7d
IvanShabunin379 <IvanShabunin379@yandex.ru>
2024-02-28 23:04:32 +0300

    add hw2

--- scrapper/src/main/java/edu/java/clients/GitHubClient.java
+++ scrapper/src/main/java/edu/java/clients/GitHubClient.java
@@ -2,9 +2,28 @@ package edu.java.clients;
 
 import edu.java.responses.GitHubResponse;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.web.reactive.function.client.WebClient;
 
 public class GitHubClient {
+    private static final String BASE_URL = "https://api.github.com";
+    private final WebClient webClient;
+
+    public GitHubClient() {
+        this.webClient = WebClient.create(BASE_URL);
+    }
+
+    public GitHubClient(String url) {
+        this.webClient = WebClient.create(url);
+    }
+
     public GitHubResponse getRepositoryUpdate(@NotNull String owner, @NotNull String repository) {
+        webClient.get()
+            .uri("repos/{owner}/{repo}", owner, repository)
+            .retrieve()
+            .bodyToMono(GitHubResponse.class)
+            .block();
+        //.onStatus(HttpStatus::is4xxClientError, clientResponse -> )
+
         return null;
     }
 }
--- scrapper/src/main/java/edu/java/clients/StackOverflowClient.java
+++ scrapper/src/main/java/edu/java/clients/StackOverflowClient.java
@@ -1,8 +1,20 @@
 package edu.java.clients;
 
 import edu.java.responses.StackOverflowResponse;
+import org.springframework.web.reactive.function.client.WebClient;
 
 public class StackOverflowClient {
+    private static final String BASE_URL = "https://api.stackexchange.com";
+    private final WebClient webClient;
+
+    public StackOverflowClient() {
+        this.webClient = WebClient.create(BASE_URL);
+    }
+
+    public StackOverflowClient(String url) {
+        this.webClient = WebClient.create(url);
+    }
+
     public StackOverflowResponse getQuestionUpdate(long questionId) {
         return null;
     }
