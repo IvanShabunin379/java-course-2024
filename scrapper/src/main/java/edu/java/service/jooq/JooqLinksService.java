3806d114d5f0abddd67687c414f99c6c8ac9e35d
IvanShabunin379 <IvanShabunin379@yandex.ru>
2024-04-09 00:37:38 +0300

    add hw5-bonus

---
+++ scrapper/src/main/java/edu/java/service/jooq/JooqLinksService.java
@@ -0,0 +1,28 @@
+package edu.java.service.jooq;
+
+import edu.java.domain.model.Link;
+import edu.java.service.LinksService;
+import java.net.URI;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class JooqLinksService implements LinksService {
+    @Override
+    public Link add(long tgChatId, URI url) {
+        return null;
+    }
+
+    @Override
+    public Link remove(long tgChatId, URI url) {
+        return null;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Link> listAll(long tgChatId) {
+        return null;
+    }
+}
