844c43c037e72b3d642d3359c20807168aaf54b0
IvanShabunin379 <IvanShabunin379@yandex.ru>
2024-04-10 01:00:00 +0300

    add JpaTgChatsService

--- scrapper/src/main/java/edu/java/service/jpa/JpaTgChatsService.java
+++ scrapper/src/main/java/edu/java/service/jpa/JpaTgChatsService.java
@@ -1,19 +1,42 @@
 package edu.java.service.jpa;
 
+import edu.java.domain.model.jpa.TgChatEntity;
+import edu.java.domain.repository.jdbc.JdbcTgChatsRepository;
+import edu.java.domain.repository.jpa.JpaTgChatsRepository;
+import edu.java.exceptions.TgChatAlreadyExistsException;
+import edu.java.exceptions.TgChatNotFoundException;
 import edu.java.service.TgChatsService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.time.OffsetDateTime;
 
 @Service
 @Transactional
 public class JpaTgChatsService implements TgChatsService {
+    private final JpaTgChatsRepository tgChatsRepository;
+
+    public JpaTgChatsService(JpaTgChatsRepository tgChatsRepository) {
+        this.tgChatsRepository = tgChatsRepository;
+    }
+
     @Override
     public void register(long tgChatId) {
+        if (tgChatsRepository.existsById(tgChatId)) {
+            throw new TgChatAlreadyExistsException();
+        }
 
+        TgChatEntity newTgChat = new TgChatEntity();
+        newTgChat.setId(tgChatId);
+        newTgChat.setCreatedAt(OffsetDateTime.now());
+
+        tgChatsRepository.save(newTgChat);
     }
 
     @Override
     public void unregister(long tgChatId) {
+        TgChatEntity unregistredChat = tgChatsRepository.findById(tgChatId)
+            .orElseThrow(TgChatNotFoundException::new);
 
+        tgChatsRepository.delete(unregistredChat);
     }
 }
