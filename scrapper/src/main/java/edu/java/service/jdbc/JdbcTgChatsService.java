a4fef3ef4326ae4125b865a3c5bcdb050f959e6e
IvanShabunin379 <IvanShabunin379@yandex.ru>
2024-04-11 02:06:14 +0300

    fix CheckStyle

--- scrapper/src/main/java/edu/java/service/jdbc/JdbcTgChatsService.java
+++ scrapper/src/main/java/edu/java/service/jdbc/JdbcTgChatsService.java
@@ -5,15 +5,15 @@ import edu.java.domain.model.TgChat;
 import edu.java.domain.repository.jdbc.JdbcLinksRepository;
 import edu.java.domain.repository.jdbc.JdbcLinksTrackingsRepository;
 import edu.java.domain.repository.jdbc.JdbcTgChatsRepository;
+import edu.java.service.TgChatsService;
 import edu.java.service.exceptions.LinkNotFoundException;
 import edu.java.service.exceptions.TgChatAlreadyExistsException;
 import edu.java.service.exceptions.TgChatNotFoundException;
-import edu.java.service.TgChatsService;
+import java.net.URI;
+import java.util.List;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.net.URI;
-import java.util.List;
 
 @Service
 @Transactional
@@ -22,7 +22,11 @@ public class JdbcTgChatsService implements TgChatsService {
     private final JdbcLinksRepository linksRepository;
     private final JdbcLinksTrackingsRepository linksTrackingsRepository;
 
-    public JdbcTgChatsService(JdbcTgChatsRepository tgChatsRepository, JdbcLinksRepository linksRepository, JdbcLinksTrackingsRepository linksTrackingsRepository) {
+    public JdbcTgChatsService(
+        JdbcTgChatsRepository tgChatsRepository,
+        JdbcLinksRepository linksRepository,
+        JdbcLinksTrackingsRepository linksTrackingsRepository
+    ) {
         this.tgChatsRepository = tgChatsRepository;
         this.linksRepository = linksRepository;
         this.linksTrackingsRepository = linksTrackingsRepository;
