e214d48c9a8563b569ca1654470d101d99136d6b
IvanShabunin379 <IvanShabunin379@yandex.ru>
2024-04-12 23:57:15 +0300

    fix Build

--- scrapper/src/main/java/edu/java/service/jooq/JooqTgChatsService.java
+++ scrapper/src/main/java/edu/java/service/jooq/JooqTgChatsService.java
@@ -1,8 +1,11 @@
 package edu.java.service.jooq;
 
+import edu.java.domain.model.TgChat;
 import edu.java.service.TgChatsService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.net.URI;
+import java.util.List;
 
 @Service
 @Transactional
@@ -15,5 +18,10 @@ public class JooqTgChatsService implements TgChatsService {
     @Override
     public void unregister(long tgChatId) {
 
+    }
+
+    @Override
+    public List<TgChat> listAll(URI linkUri) {
+        return null;
     }
 }
