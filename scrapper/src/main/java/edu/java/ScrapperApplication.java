c32b68355ed0f809b7480e9bf55373c2de659d62
IvanShabunin379 <IvanShabunin379@yandex.ru>
2024-04-27 00:30:40 +0300

    fix hw7

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -5,11 +5,13 @@ import edu.java.configuration.ScrapperAppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ScrapperAppConfig.class, DatabaseConfig.class})
 @EnableScheduling
+@EnableCaching
 public class ScrapperApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScrapperApplication.class, args);
