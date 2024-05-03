33b68c1e7befd24d1cbec1b300f8c24bd097667d
IvanShabunin379 <IvanShabunin379@yandex.ru>
2024-05-03 22:58:46 +0300

    add ProcessedTelegramMessagesCounter

--- bot/src/main/java/edu/java/bot/configuration/BotInitializer.java
+++ bot/src/main/java/edu/java/bot/configuration/BotInitializer.java
@@ -4,7 +4,6 @@ import edu.java.bot.service.TelegramBot;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
